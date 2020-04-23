10e7e3b0610dace93b548e560423438eb2bcb1d3
Hristijan Petreski <xyan@outlook.com>
2020-04-23 16:34:43 +0200

    Remove sub-project git Add missing files

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            In anoter Page
+            <Link to="/">Go Home</Link>
+        </div>
+    );
+};
